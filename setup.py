1585fc000a400e520b6201d35d2e8c7e44d7170e
Malte Franken <exxamalte@users.noreply.github.com>
2020-06-25 03:06:36 +1000

    Python 3.8 support (#3)
    * added support for python 3.8
    
    * clean up

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+NAME = "georss_ingv_centro_nazionale_terremoti_client"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = "A GeoRSS client library for the INGV Centro Nazionale Terremoti (Earthquakes) feed."
+URL = "https://github.com/exxamalte/python-georss-ingv-centro-nazionale-terremoti-client"
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -8,18 +14,19 @@ REQUIRES = [
 ]
 
 setup(
-    name="georss_ingv_centro_nazionale_terremoti_client",
+    name=NAME,
     version="0.1",
-    author="Malte Franken",
-    author_email="coding@subspace.de",
-    description="A GeoRSS client library for the INGV Centro Nazionale Terremoti (Earthquakes) feed.",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/exxamalte/python-georss-ingv-centro-nazionale-terremoti-client",
+    url=URL,
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
